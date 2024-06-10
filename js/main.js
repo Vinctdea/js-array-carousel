12aaf331ad3996fc81acbad1ae62280a918a0120
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-10 16:25:32 +0200

    feat: add button and class css

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+// Dato un array contenente una lista di cinque immagini
+const imgData = ["img/01.webp", "img/02.webp", "img/03.webp","img/04.webp", "img/05.webp"];
+console.log(imgData);
+const container = document.querySelector(".container")
+
+// creare ciclo per leggere array 
+for (let i = 0; i < imgData.length; i++) {
+    let numeroImg = imgData[i];
+    console.log(numeroImg); 
+
+    // generare elemento div con img
+    let contenuto = `
+    <div class="item">
+        <img src="${numeroImg}">
+    </div>`;
+    
+    // inserisco i div+img creati
+    container.innerHTML += contenuto;
+}
+
+// seleziono la lista
+
+const items = document.getElementsByClassName("item");
+let itemAttivi = 0;
+
+
+// aggiungo la classe item
+items[itemAttivi].classList.add("active");
+
+// associo bottoni
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+// click next
+
